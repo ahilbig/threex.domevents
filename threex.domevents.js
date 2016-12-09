22f761c5193ba37d4f472321381178a539178d88
breeze4 <ambiturner0@gmail.com>
2016-12-08 20:36:07 -0800

    added variable declaration

--- threex.domevents.js
+++ threex.domevents.js
@@ -450,4 +450,5 @@ THREEx.DomEvents.prototype._onTouchEvent	= function(eventName, domEvent)
 	this._onEvent(eventName, mouseX, mouseY, domEvent);	
 }
 
+let DomEvents;
 export default DomEvents = THREEx.DomEvents;
